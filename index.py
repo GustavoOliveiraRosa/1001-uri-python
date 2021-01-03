935a4609a974bef57186794f9f8a6d799e7db330
Gustavo <gustavoolrosa2019@gmail.com>
2021-01-03 15:07:58 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 # Capturando o valor e inserindo nas variáves com a função input
-A = input("Digite o valor A:")
-B = input("Digite o valor B:")
+A = int(input("Digite o valor A:"))
+B = int(input("Digite o valor B:"))
 # Efetuando a soma dos valores contidos em A e B e armazenando em X
 X = A + B
 # Printando o resultado
